63b5a9724aa879e13954d26ba62d156c27d1dd74
EarlWSC <ocdaoch@wsc.school.nz>
2025-10-14 09:53:44 +1300

    infopage dropdown update

--- index.js
+++ index.js
@@ -23,67 +23,67 @@ const quizQuestions = [
         question: "Which manufacturer supplies engines for all Formula 2 cars?",
         answers: ["Renault", "Cosworth", "Honda", "Mecachrome"],
         correct: 3,
-        img: "images/f2_engine_quiz.jpg"
+        img: "images/f2_quiz.jpg"
     },
     {
         question: "Which race is the crown jewel of the IndyCar Series?",
         answers: ["Daytona 500", "Indy 500", "Long Beach GP", "Texas GP"],
         correct: 1,
-        img: "images/indy500_quiz.jpg"
+        img: "images/indycar_quiz.jpg"
     },
     {
         question: "Which class in WEC features hybrid-powered prototypes?",
         answers: ["GT3", "Hypercar", "LMP2", "GTE-Pro"],
         correct: 1,
-        img: "images/wec_hypercar_quiz.jpg"
+        img: "images/wec_quiz.jpg"
     },
     {
         question: "What is the top class in IMSA as of 2024?",
         answers: ["GTLM", "DPi", "GTP", "LMP1"],
         correct: 2,
-        img: "images/imsa_gtp_quiz.jpg"
+        img: "images/imsa_quiz.jpg"
     },
     {
         question: "Which driver has the most NASCAR Cup Series championships?",
         answers: ["Jeff Gordon", "Richard Petty", "Dale Earnhardt", "Jimmie Johnson"],
         correct: 3,
-        img: "images/nascar_champs_quiz.jpg"
+        img: "images/nascar_quiz.jpg"
     },
     {
         question: "Which car class is used in GT World Challenge?",
-        answers: ["LMP1", "GT3", "Touring Cars", "Formula Cars"],
+        answers: ["GT4", "GT3", "Touring Cars", "GT2"],
         correct: 1,
-        img: "images/gtworld_gt3_quiz.jpg"
+        img: "images/gtwc_quiz.jpg"
     },
     {
         question: "What surface is Rally Sweden primarily run on?",
         answers: ["Gravel", "Asphalt", "Snow and Ice", "Mixed Terrain"],
         correct: 2,
-        img: "images/wrc_sweden_quiz.jpg"
+        img: "images/wrc_quiz.jpg"
     },
     {
         question: "What is unique about Rallycross circuits?",
-        answers: ["They combine gravel and tarmac","They are run entirely on snow", "They are only street circuits", "They use electric cars exclusively"],
+        answers: ["They combine gravel and tarmac","They are wide circuits", "They are only run during the rain", "They use gravel, sand and tarmac"],
         correct: 0,
-        img: "images/wrx_surface_quiz.jpg"
+        img: "images/wrx_quiz.jpg"
     },
     {
         question: "What is the engine displacement of MotoGP bikes?",
         answers: ["500cc", "800cc", "1000cc", "1200cc"],
         correct: 2,
-        img: "images/motogp_engine_quiz.jpg"
+        img: "images/motogp_quiz.jpg"
     },
     {
         question: "What is the primary judging criteria in Formula Drift?",
-        answers: ["Lap time", "Tire wear", "Fuel efficiency","Angle, speed, and style"],
+        answers: ["Laptimes and style", "Tire Smoke size", "Position against opponent","Angle, speed, and style"],
         correct: 3,
-        img: "images/formuladrift_style_quiz.jpg"
+        img: "images/fd_quiz.jpg"
     },
     {
         question: "What is the fastest class in NHRA drag racing?",
         answers: ["Top Fuel", "Funny Car", "Pro Stock", "Super Comp"],
         correct: 0,
-        img: "images/nhra_topfuel_quiz.jpg"
+        img: "images/nhra_quiz.jpg"
     }
 ];
 
